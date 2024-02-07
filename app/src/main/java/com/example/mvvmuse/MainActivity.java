dc7fe1e06a7850ddd06de4259ab9b2d72a3ef69b
samet.uyanik <samet.uyanik2756@gmail.com>
2024-02-07 12:51:41 +0300

    Event Handle

--- app/src/main/java/com/example/mvvmuse/MainActivity.java
+++ app/src/main/java/com/example/mvvmuse/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.mvvmuse;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
 
@@ -11,36 +12,30 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding=ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+        //binding=ActivityMainBinding.inflate(getLayoutInflater());
+        binding= DataBindingUtil.setContentView(this,R.layout.activity_main);
 
         binding.textViewSonuc.setText("0");
+        binding.setMainActivityNesnesi(this);
 
 
-        binding.buttonTopla.setOnClickListener(v -> {
-            String alinanSayi1=binding.editTextSayi1.getText().toString();
-            String alinanSayi2=binding.editTextSayi2.getText().toString();
+    }
+    public void buttonToplamaTikla(String alinanSayi1,String alinanSayi2){
 
-            int sayi1=Integer.parseInt(alinanSayi1);
-            int sayi2=Integer.parseInt(alinanSayi2);
+        int sayi1=Integer.parseInt(alinanSayi1);
+        int sayi2=Integer.parseInt(alinanSayi2);
 
-            int toplam=sayi1+sayi2;
+        int toplam=sayi1+sayi2;
 
-            binding.textViewSonuc.setText(String.valueOf(toplam));
+        binding.textViewSonuc.setText(String.valueOf(toplam));
+    }
+    public void buttonCarpmaTikla(String alinanSayi1,String alinanSayi2){
 
+        int sayi1=Integer.parseInt(alinanSayi1);
+        int sayi2=Integer.parseInt(alinanSayi2);
 
-        });
+        int carpma= sayi1 * sayi2;
 
-        binding.buttonCarpma.setOnClickListener(v -> {
-            String alinanSayi1=binding.editTextSayi1.getText().toString();
-            String alinanSayi2=binding.editTextSayi2.getText().toString();
-
-            int sayi1=Integer.parseInt(alinanSayi1);
-            int sayi2=Integer.parseInt(alinanSayi2);
-
-            int carpma= sayi1 * sayi2;
-
-            binding.textViewSonuc.setText(String.valueOf(carpma));
-        });
+        binding.textViewSonuc.setText(String.valueOf(carpma));
     }
 }
